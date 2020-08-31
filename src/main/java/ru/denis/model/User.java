ff5c9ed0aa62b9a9793f028e45d62f8b195d0269
moshkovdenis <moshkovdenis@mail.ru>
2020-08-31 19:10:01 +0300

    refactor

--- src/main/java/ru/denis/model/User.java
+++ src/main/java/ru/denis/model/User.java
@@ -1,12 +1,11 @@
 package ru.denis.model;
 
 import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Objects;
+
 
 @Entity
 @Table(name = "user")
-public class User implements Serializable {
+public class User  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,12 +20,6 @@ public class User implements Serializable {
     private String mail;
 
     public User() {
-    }
-
-    public User(String name, String surname, String mail) {
-        this.name = name;
-        this.surname = surname;
-        this.mail = mail;
     }
 
     public Long getId() {
@@ -59,28 +52,5 @@ public class User implements Serializable {
 
     public void setMail(String mail) {
         this.mail = mail;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) &&
-                Objects.equals(name, user.name) &&
-                Objects.equals(surname, user.surname) &&
-                Objects.equals(mail, user.mail);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, surname, mail);
     }
 }
