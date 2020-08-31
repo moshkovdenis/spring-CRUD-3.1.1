eb6f611267ed5fbba2ea2dc17393c1fb255b9be9
moshkovdenis <moshkovdenis@mail.ru>
2020-08-31 21:49:08 +0300

    refactor

--- src/main/java/ru/denis/config/AppInit.java
+++ src/main/java/ru/denis/config/AppInit.java
@@ -33,7 +33,7 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{JpaConfig.class};
+        return null;
     }
 
 
